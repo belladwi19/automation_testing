2c106e84b0ebe29e6fa58c9387b5522347c300f6
belladwi19 <79824022+belladwi19@users.noreply.github.com>
2023-02-18 11:49:52 +0700

    automation tes

--- factorial.cy.js
+++ factorial.cy.js
@@ -4,16 +4,16 @@ describe('factorial', () => {
   it('passes', () => {
     cy.visit('https://qa.putraprima.id/')
     cy.get('input[name=input]').type(faker.random.numeric(2))
-    cy.contains('hitung').click()
+    cy.contains('Hitung Faktorial').click()
   })
   it('false', () => {
     cy.visit('https://qa.putraprima.id/')
     cy.get('input[name=input]').type(faker.random.alpha(2))
-    cy.contains('hitung').click()
+    cy.contains('Hitung Faktorial').click()
   })
   it('false', () => {
     cy.visit('https://qa.putraprima.id/')
     cy.get('input[name=input]').type(faker.random.numeric)
-    cy.contains('hitung').click()
+    cy.contains('Hitung Faktorial').click()
   })
 })
